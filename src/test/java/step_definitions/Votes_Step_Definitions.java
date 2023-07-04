cb082fb1ab0b86f10efbec140eb76efe1371c896
bsaitkulov <bsaitkulov@gmail.com>
2023-07-04 20:36:58 +0400

    Votes_Step_definitions modified

--- src/test/java/step_definitions/Votes_Step_Definitions.java
+++ src/test/java/step_definitions/Votes_Step_Definitions.java
@@ -27,34 +27,34 @@ public class Votes_Step_Definitions{
     private VotesController votesController;
 
     @Given("I have a base URL {string} with header key {string} and header value {string}")
-    public void i_have_a_base_url_with_header_key_and_header_value(String baseUrl, String headerKey, String headerValue) {
+    public void setBaseUrlAndHeaders(String baseUrl, String headerKey, String headerValue) {
         votesController = new VotesController(baseUrl, headerKey, headerValue);
         
     }
     @When("I send GET request with endpoint {string}")
-    public void i_send_get_request_with_endpoint(String endpoint) {
+    public void sendGetRequest(String endpoint) {
         response = votesController.get(endpoint);
     }
 
     @Then("I have to store responseBody to unmarshall")
-    public void i_have_to_store_response_body_to_unmarshall() {
+    public void storeResponseBodyToUnmarshall() {
         votes = response.getBody().as(Votes[].class);
     }
 
     @Then("I should verify status code is {int}")
-    public void i_should_verify_status_code_is(int expectedStatusCode) {
+    public void verifyStatusCode(int expectedStatusCode) {
         int actualStatusCode = response.getStatusCode();
         Assert.assertEquals(expectedStatusCode, actualStatusCode);
     }
 
     @Then("I should verify the response result is more than {int}")
-    public void i_should_verify_the_response_result_is_more_than(int zero) {
+    public void verifyResponseResult(int zero) {
         int resultLength = votes.length;
         Assert.assertTrue(resultLength > zero);
     }
 
     @When("i select a random voteId from the stored votes")
-    public void i_select_a_random_vote_id_from_the_stored_votes() {
+    public void selectRandomVoteId() {
         Random random = new Random();
         int index = random.nextInt(votes.length);
         Votes selectedId = votes[index];
@@ -62,19 +62,19 @@ public class Votes_Step_Definitions{
     }
 
     @When("I send GET request to {string} with random element")
-    public void i_send_get_request_to_with_random_element(String endpoint) {
+    public void sendGetRequestWithRandomVoteId(String endpoint) {
        String updatedEndpoint = endpoint.replace("{id}", randomId);
        response = votesController.get(updatedEndpoint);
     }
     
     @Then("the response should not be empty")
-    public void the_response_should_not_be_empty() {
+    public void verifyResponseNotEmpty() {
         Votes voteBody = response.getBody().as(Votes.class);
         Assert.assertNotNull(voteBody);
     }
 
     @Then("the fields in the response should match the corresponding given object")
-    public void the_fields_in_the_response_should_match_the_corresponding_given_object() {
+    public void verifyResponseFieldsMatchGivenObject() {
         Votes expectedVote = Arrays.stream(votes).filter(v -> v.getId().equals(randomId))
                 .findFirst()
                 .orElse(null);
@@ -83,37 +83,37 @@ public class Votes_Step_Definitions{
     }
     
     @When("I create a new vote using POST request to {string}")
-    public void i_create_a_new_vote_using_post_request_to(String endpoint) throws JsonProcessingException {
+    public void createNewVote(String endpoint) throws JsonProcessingException {
         String voteJson = ObjectConverter.convertJavaToJson(EntitiesManager.createVote());
         response = votesController.post(endpoint, voteJson);
     }
     
     @Then("the response status code should be {int}")
-    public void the_response_status_code_should_be(int expectedStatusCode) {
+    public void verifyResponseStatusCode(int expectedStatusCode) {
         int actualStatusCode = response.getStatusCode();
         Assert.assertEquals(expectedStatusCode, actualStatusCode);
     }
 
     @Then("the response match expected value message {string}")
-    public void the_response_match_expected_value_message(String expectedMessage) {
+    public void verifyResponseMatchesExpectedValue(String expectedMessage) {
        newVote = response.getBody().as(Votes.class);
        String actualMessage = newVote.getMessage();
        Assert.assertEquals(expectedMessage, actualMessage);
     }
 
     @Then("the id response is not empty")
-    public void the_response_is_not_empty() {
+    public void verifyIdResponseNotEmpty() {
        Assert.assertNotNull(newVote.getId());
     }
 
     @When("I send GET request with new id to {string}")
-    public void i_send_get_request_with_new_id_to(String endpoint) {
+    public void sendGetRequestWithNewVoteId(String endpoint) {
         String updatedEndpoint = endpoint.replace("{id}", newVote.getId());
         response = votesController.get(updatedEndpoint);
     }
     
     @Then("the id response should match id request")
-    public void the_id_response_should_match_id_request() {
+    public void verifyIdResponseMatchesRequest() {
        Votes responseBody = response.getBody().as(Votes.class);
        String actualId = responseBody.getId();
        String expectedId = newVote.getId();
@@ -121,26 +121,26 @@ public class Votes_Step_Definitions{
     }
     
     @When("I send DELETE request with same id to {string}")
-    public void i_send_delete_request_with_same_id_to(String endpoint) {
+    public void sendDeleteRequestWithNewVoteId(String endpoint) {
         String updatedEndpoint = endpoint.replace("{id}", newVote.getId());
         response = votesController.delete(updatedEndpoint);
     }
     
     @Then("the response body should match the expected value message {string}")
-    public void the_response_body_should_match_the_expected_value_message(String expectedMessage) {
+    public void verifyResponseBodyMatchesExpectedValue(String expectedMessage) {
         deletedVote = response.getBody().as(Votes.class);
         String actualMessage = deletedVote.getMessage();
         Assert.assertEquals(expectedMessage, actualMessage);
     }
 
     @When("I send a GET request to {string}")
-    public void i_send_a_get_request_to(String endpoint) {
+    public void sendGetRequestWithVoteId(String endpoint) {
         String updatedEndpoint = endpoint.replace("{id}", newVote.getId());
         response = votesController.get(updatedEndpoint);
     }
 
     @Then("the response should match the expected value messageBody {string}")
-    public void the_response_should_match_the_expected_value_message_body(String expectedMessage) {
+    public void verifyResponseMatchesExpectedValueNotFound(String expectedMessage) {
         String actualMessage = response.getBody().asString();
         Assert.assertEquals(expectedMessage, actualMessage);
     }
